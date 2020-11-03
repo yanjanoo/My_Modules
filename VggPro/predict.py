1a87c63487b60c49bc3189c349398ef7981bc4e1
yanjanoo <48510391+yanjanoo@users.noreply.github.com>
2020-11-03 10:12:26 +0800

    Create predict.py

---
+++ VggPro/predict.py
@@ -0,0 +1,41 @@
+import torch
+from vggPro import VggPro
+from PIL import Image
+from torchvision import transforms
+import matplotlib.pyplot as plt
+import json
+
+data_transform = transforms.Compose(
+    [transforms.Resize((224, 224)),
+     transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+# load image
+img = Image.open("D:/DataSet/flower_data/sunflower.jpg")
+plt.imshow(img)
+# [N, C, H, W]
+img = data_transform(img)
+# expand batch dimension
+img = torch.unsqueeze(img, dim=0)
+
+# read class_indict
+try:
+    json_file = open('D:/DataSet/flower_data/class_indices.json', 'r')
+    class_indict = json.load(json_file)
+except Exception as e:
+    print(e)
+    exit(-1)
+
+# create model
+model = VggPro(num_classes=5)
+# load model weights
+model_weight_path = "./VggProNet.pth"
+model.load_state_dict(torch.load(model_weight_path))
+model.eval()
+with torch.no_grad():
+    # predict class
+    output = torch.squeeze(model(img))
+    predict = torch.softmax(output, dim=0)
+    predict_cla = torch.argmax(predict).numpy()
+print(class_indict[str(predict_cla)])
+plt.show()
